1b1d007072954ba0ad14a7c226865e78faeb6496
oga0927 <totti0927.oga@gmail.com>
2021-02-26 21:58:54 +0900

    test

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const todoList = []
+let inputForm, todoMain, tabButton, sortMenu
+
+// DOMを変数に登録する
+function regesterDom() {
+  inputform = document.querySelector("#input-form")
+  todoMain = document.querySelector("todo-main")
+  tabButton = document.querySelector("#tab").querySelectorAll("button")
+  sortMenu = document.querySelector("#sort-menu")
+}
+
+// 初期化
+function initialize() {
+  registerDom()
+}
+
+document.addEventListener("DOMContentLoaded", initialize.bind(this))
+aaaaaadcfrtghdgfjfdghd 

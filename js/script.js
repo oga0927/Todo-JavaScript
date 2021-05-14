842d21f0936b35d552b524522161d27fb1ba1b37
OGA <totti0927.oga@gmail.com>
2021-05-14 20:30:36 +0900

    イベント登録

--- js/script.js
+++ js/script.js
@@ -8,9 +8,26 @@ function registerDom() {
   sortMenu = document.querySelectorAll("#sort-menu")
 }
 
-// 初期化
+// 初期化 関数の呼び出し
 function initialize() {
   registerDom()
+  bindEvents()
 }
 
 document.addEventListener("DOMContentLorded", initialize.bind(this))
+
+// DOMにイベントを設定
+/* フォームタグで登録ボタンをクリックしたらhandle関数を実行せよ */
+function bindEvents() {
+  inputForm.addEventListener("submit", () => handleSubmit())
+}
+
+/* Todoを登録する処理 */
+function handleSubmit(event) {
+  // ページ遷移を止める
+  event.preventDefault()
+  const todoObj = {
+    text: inputForm["input-text"].value
+  }
+  addTodo(todoObj)
+}
